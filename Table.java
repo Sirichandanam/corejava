4073078919a3093a255c8a1403b19474ae63c144
Sirichandanam <107123095+Sirichandanam@users.noreply.github.com>
2022-06-09 10:44:35 +0530

    second commit

---
+++ Table.java
@@ -0,0 +1,26 @@
+package corejava;
+import java.util.Scanner;
+
+public class Table {
+	public static void main(String[] args) {
+		
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter the Number");
+	int n=in.nextInt();
+	for(int i=1;i<=10;i++)
+	{
+	System.out.println(n+"*"+i+"="+(n*i));	
+		
+		
+	
+		// TODO Auto-generated method stub
+
+	}
+	
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ variableTypes.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class variableTypes {
+	int b=5;
+	public static void main(String[] args) {
+		int a=10;
+		variableTypes v1=new variableTypes();
+		v1.m1();
+		System.out.println("A="+a);
+		
+	}
+	public void m1() {
+		System.out.println("the value A is :"+b);
+	}
+
+}
