6b9e05744c94ebfe2b3ec41eec0421b0b9a7b773
Sirichandanam <107123095+Sirichandanam@users.noreply.github.com>
2022-06-09 13:00:47 +0530

    d drive

---
+++ Add.java
@@ -0,0 +1,14 @@
+package sirichandana;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println("Sum" + "="+ c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
